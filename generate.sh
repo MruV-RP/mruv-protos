b496e48c61f2556898a14e1cdab5baf8c3f7921c
Mrucznik <mrucznix@gmail.com>
2019-10-12 02:04:20 +0200

    Docs in ./gen/docs

--- generate.sh
+++ generate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
+mkdir -p /gen/docs
 mkdir -p /gen/swagger
 mkdir -p /gen/pb-go
 
 for x in /protos/**/*.proto; do protoc -I/opt/include --proto_path=/protos --swagger_out=logtostderr=true:/gen/swagger --go_out=plugins=grpc,paths=source_relative:/gen/pb-go $x; done
+
+protoc -I/opt/include --proto_path=/protos --doc_opt=markdown,docs.md --doc_out=/gen/docs /protos/**/*.proto
