b78038f9a17ab39e43dbcb48f1641d56313fe836
Mrucznik <mrucznix@gmail.com>
2020-11-10 21:14:58 +0100

    Auto Creation of Proto v0.8.3

--- generate.sh
+++ generate.sh
@@ -5,6 +5,6 @@ mkdir -p /gen/pb-docs
 mkdir -p /gen/pb-swagger
 mkdir -p /gen/pb-go
 
-for x in /protos/**/*.proto; do protoc -I/opt/include --proto_path=/protos --swagger_out=logtostderr=true:/gen/pb-swagger --go_out=plugins=grpc,paths=source_relative:/gen/pb-go --go_opt paths=source_relative:/gen/pb-go --grpc-gateway_opt paths=source_relative:/gen/pb-go --grpc-gateway_out /gen/pb-go --grpc-gateway_opt logtostderr=true --validate_out=lang=go,paths=source_relative:/gen/pb-go $x; done
+for x in /protos/**/*.proto; do protoc -I/opt/include --proto_path=/protos --swagger_out=logtostderr=true:/gen/pb-swagger --go_out=plugins=grpc,paths=source_relative:/gen/pb-go --go_opt paths=source_relative --grpc-gateway_opt paths=source_relative --grpc-gateway_out /gen/pb-go --grpc-gateway_opt logtostderr=true --validate_out=lang=go,paths=source_relative:/gen/pb-go $x; done
 
 protoc -I/opt/include --proto_path=/protos --doc_opt=markdown,docs.md --doc_out=/gen/pb-docs /protos/**/*.proto
